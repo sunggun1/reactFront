9f3870db12efccbb603a3079a4b5e73cd2acc301
sunggun1 <readingdeveloper@naver.com>
2023-06-18 22:34:17 +0900

    api 변경

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost"){
     backendHost = "http://localhost:8080";
 }else{
-    backendHost = "http://13.124.30.40:8080";
+    backendHost = "http://api.memoproject1.com:8080";
 }
 
 export const API_BASE_URL = `${backendHost}`;
